2b214326071388dc8fcb2bac6f4a853d4841f6b8
kuzivgalya <kuzivgm@gmail.com>
2021-01-18 18:14:02 +0200

    Lesson3_6

---
+++ Lesson3_6.py
@@ -0,0 +1,57 @@
+#1. Создать ф-цию которая будет возвращать группу детей в качестве списка из словарей
+#2. Создать ф-цию которая будет возвращать группу детей в качестве списка из классов
+
+
+from pprint import pprint
+
+def task_1(amount_of_kids: int) -> list:
+    """Returns list of dictionaries"""
+
+    group_of_kids = []
+
+    for i in range(amount_of_kids):
+        kid = {}
+        print('Kid ', i+1, ':')
+        kid["name"] = input('Enter name: ')
+        kid["surname"] = input('Enter surname: ')
+        kid["age"] = input('Enter age: ')
+        group_of_kids.append(kid)
+
+    return group_of_kids
+
+class Person:
+    pass
+#   def __init__(self, name, surname, age):
+#       self.name = name
+#       self.surname = surname
+#       self.age = age
+
+def task_2(amount_of_kids: int) -> list:
+    """Returns list of classes"""
+
+    group_of_kids = []
+
+    for i in range(amount_of_kids):
+        kid = Person()
+        print('Kid ', i+1, ':')
+        kid.name = input('Enter name:')
+        kid.surname = input('Enter surname:')
+        kid.age = input('Enter age:')
+        group_of_kids.append(kid)
+
+    return group_of_kids
+
+
+#Get results
+#==========================================
+task_1_result = task_1(amount_of_kids=3)
+task_2_result = task_2(amount_of_kids=3)
+
+
+#Print results
+#==========================================
+print('Task 1:')
+pprint(task_1_result)
+print('Task 2:')
+pprint(task_2_result)
+
